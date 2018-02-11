8ec718335f842af2f94c2502e45acbc7c11bdb54
Mario Aliaga <>
2018-02-10 22:06:57 -0300

    Agrego linea base

---
+++ src/pages/subir/subir.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {ViewController} from "ionic-angular";
+
+@Component({
+  selector: 'page-subir',
+  templateUrl: 'subir.html',
+})
+export class SubirPage {
+
+  constructor(private viewCtrl: ViewController) {
+  }
+
+  cerrarModal() {
+    this.viewCtrl.dismiss();
+  }
+}
